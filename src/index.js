cf2ec4277a5cefe8d5dab9f4eb6a4afaecce87f8
Lavanya S <lavanya.sethuraman23@gmail.com>
2017-09-11 21:42:03 -0400

    first trial

--- src/index.js
+++ src/index.js
@@ -4,13 +4,11 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import ExpenseManager from './components/ExpenseManager';
 import './index.css';
 
- 
 const App = () => (
   <MuiThemeProvider>
     <ExpenseManager />
   </MuiThemeProvider>
 );
- 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
