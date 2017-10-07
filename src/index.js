5bc95dc949b3ae658436cb1e36e64d1eea9f1f5a
Lavanya S <lavanya.sethuraman23@gmail.com>
2017-10-07 14:22:51 -0400

    full version client

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import store from './store';
-import ExpenseManager from '../src/components/expense-manager';
+import ExpenseManager from '../src/containers/expense-manager';
 import './index.css';
 
 
